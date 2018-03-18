6b8f53b8cc276019de5f390e450f5b22f312aefc
tanhanliang <tanhanliang@github.com>
2018-03-18 00:02:33 +0000

    Add generator architecture

--- models/models.py
+++ models/models.py
@@ -5,16 +5,18 @@ import keras.models as models
 import keras.layers as layers
 import keras.optimizers as opt
 import models.parameters as params
+import numpy as np
+
+IMG_SHAPE = (params.WIDTH, params.HEIGHT, params.CHANNELS)
+NOISE_SHAPE = (100,)
 
 
 def build_discriminator():
     """
-    Builds the model which classifies an image as a picture of hanliang or not.
+    Builds the model which classifies an image as a picture of han liang or not.
 
     :return: A keras Model
     """
-
-    shape = (params.WIDTH, params.HEIGHT, params.CHANNELS)
 
     # model = models.Sequential()
     # model.add(layers.Convolution2D(32, (5, 5), activation="relu", input_shape=shape))
@@ -27,7 +29,7 @@ def build_discriminator():
     # model.add(layers.Dense(1, activation="sigmoid"))
     model = models.Sequential()
 
-    model.add(layers.Flatten(input_shape=shape))
+    model.add(layers.Flatten(input_shape=IMG_SHAPE))
     model.add(layers.Dense(128))
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.6))
@@ -35,6 +37,31 @@ def build_discriminator():
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.6))
     model.add(layers.Dense(1, activation='sigmoid'))
+
+    optimiser = opt.adam(lr=0.002)
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimiser,
+                  metrics=['accuracy'])
+    return model
+
+
+def build_generator():
+    """
+    Builds the generator model, which takes as input random noise and attempts to form an
+    image that would be classified by the discriminator as a han liang.
+
+    :return: A keras Model
+    """
+
+    model = models.Sequential()
+    model.add(layers.Dense(256, input_shape=NOISE_SHAPE))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.BatchNormalization(momentum=0.8))
+    model.add(layers.Dense(512))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.BatchNormalization(momentum=0.8))
+    model.add(layers.Dense(np.prod(IMG_SHAPE), activation='tanh'))
+    model.add(layers.Reshape(IMG_SHAPE))
 
     optimiser = opt.adam(lr=0.002)
     model.compile(loss='binary_crossentropy',
