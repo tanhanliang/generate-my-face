6b3fbdf15a16250740b2cfbda43f0e923d6f9575
tanhanliang <tanhanliang@github.com>
2018-03-19 13:45:06 +0000

    Update model architecture

--- models/models.py
+++ models/models.py
@@ -16,29 +16,31 @@ def build_discriminator():
     """
 
     # model = models.Sequential()
-    # model.add(layers.Convolution2D(32, (5, 5), activation="relu", input_shape=shape))
+    # model.add(layers.Convolution2D(32, (5, 5), activation="relu", input_shape=params.IMG_SHAPE))
     # model.add(layers.MaxPooling2D(pool_size=(5, 5)))
-    # model.add(layers.Convolution2D(32, (5, 5), activation="relu", input_shape=shape))
+    # model.add(layers.Convolution2D(32, (5, 5), activation="relu")
     # model.add(layers.MaxPooling2D(pool_size=(5, 5)))
     # model.add(layers.Flatten())
     # model.add(layers.Dense(64, activation="relu"))
     # model.add(layers.Dense(64, activation="relu"))
     # model.add(layers.Dense(1, activation="sigmoid"))
     model = models.Sequential()
-
-    model.add(layers.Flatten(input_shape=params.IMG_SHAPE))
-    model.add(layers.Dense(128))
+    # model.add(layers.Flatten(input_shape=params.IMG_SHAPE))
+    # model.add(layers.Dense(32))
+    model.add(layers.Convolution2D(8, (5, 5), activation="relu", input_shape=params.IMG_SHAPE))
+    model.add(layers.MaxPooling2D(pool_size=(5, 5)))
     model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Flatten())
     model.add(layers.Dropout(0.6))
     model.add(layers.Dense(32))
     model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.6))
     model.add(layers.Dense(1, activation='sigmoid'))
 
-    # optimiser = opt.adam(lr=0.002)
-    # model.compile(loss='binary_crossentropy',
-    #               optimizer=optimiser,
-    #               metrics=['accuracy'])
+    optimiser = opt.adam(lr=0.002)
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimiser,
+                  metrics=['accuracy'])
     return model
 
 
